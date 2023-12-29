59ce1925d502b5bf49c9d558c64322ee7d98d406
Alexander Ioffe <deusaquilus@gmail.com>
2023-12-29 13:50:51 -0500

    Add CI stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,33 @@
 plugins {
     kotlin("jvm") version "1.9.21"
+    `java-library`
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
+    id("org.jetbrains.dokka") version "1.8.10"
+    id("signing")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
+}
+
+apply(plugin = "io.github.gradle-nexus.publish-plugin")
+
+group = "io.exoquery"
+version = "1.0"
+
+apply(plugin = "kotlin")
+apply(plugin = "maven-publish")
 
 repositories {
     mavenCentral()
+    maven(url = "https://plugins.gradle.org/m2/")
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
