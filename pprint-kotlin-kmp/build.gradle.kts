97ede8fe28659710c3c04a09e9acbbfa52c34c7f
Alexander Ioffe <deusaquilus@gmail.com>
2024-02-02 13:06:55 -0500

    Add even more targets

--- pprint-kotlin-kmp/build.gradle.kts
+++ pprint-kotlin-kmp/build.gradle.kts
@@ -17,6 +17,7 @@ kotlin {
   }
 
   linuxX64()
+  linuxArm64()
   macosX64()
   macosArm64()
   mingwX64()
@@ -26,6 +27,17 @@ kotlin {
   androidNativeArm64()
   iosX64()
   iosArm64()
+  iosSimulatorArm64()
+  tvosX64()
+  tvosArm64()
+  watchosX64()
+  watchosArm32()
+  watchosArm64()
+
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmWasi()
+  @OptIn(ExperimentalWasmDsl::class)
+  wasmJs()
 
   js {
     browser()
